1f5562deffe8d6e1ef50b78cd6525223b79e5737
Harneet Singh <harneetsingh542@gmail.com>
2026-04-07 20:31:55 +0530

    again links

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api=axios.create({
-    baseURL:"http://localhost:3000",
+    baseURL:"https://interview-prep-backend-3sza.onrender.com",
     withCredentials:true
 })
 
